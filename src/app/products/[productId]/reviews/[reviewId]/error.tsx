6a63a1682bf79f3a7778b677a1214717590f72f6
ciandre <a.stanisic.codealkonost@gmail.com>
2025-01-09 12:52:41 +0100

    Add error handling and boundary for product reviews

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import {startTransition} from 'react';
+import {useRouter} from 'next/navigation'; 
+
+
+export default function ErrorBoundary({error,reset}: {error: Error, reset: () => void}) 
+{ 
+    const r=useRouter();  
+    const reload=()=>
+    {
+        startTransition(()=>{
+            r.refresh();
+            reset();
+        }); 
+    }
+  return (
+    <div>
+        {error.message=("This is an error, try to refresh the page")}    
+        <button onClick={()=>reload()}>try to fix</button>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+
 
 async function ProductReview({params}: {params:Promise<{productId: string, reviewId: string}>})
- {   
+ {  
+  const random=Math.random();
+  if (random>=0.1)   
+  {
+    throw new Error('This is an error');  
+  }
     const { productId, reviewId} = await params;       
   return (
     <h2>
-     This is product {productId} - {reviewId}   
+     This is product {productId} -ID: {reviewId}   
     </h2>
   )
 }
